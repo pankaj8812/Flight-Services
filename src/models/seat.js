fa5ea2743a3c2ce2df08c9f70a719ee85c4d8956
Pankaj Sharma <pankajsharma376779@gamil.com>
2023-08-20 15:18:16 +0530

    Added seats model in our db associated to airplane

---
+++ src/models/seat.js
@@ -0,0 +1,45 @@
+'use strict';
+const { Enums } = require("../utils/common");
+const { BUSINESS, ECONOMY, PREMIUM_ECONOMY, FIRST_CLASS } = Enums.SEAT_TYPE;
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Seat extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      this.belongsTo(models.Airplane, {
+        foreignKey: 'airplaneId',
+      })
+    }
+  }
+  Seat.init({
+    airplanrId: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    row: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    col: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    type: {
+      type: DataTypes.ENUM,
+      values: [BUSINESS, ECONOMY, PREMIUM_ECONOMY, FIRST_CLASS],
+      defaultValue: ECONOMY,
+      allowNull: false
+    }
+  }, {
+    sequelize,
+    modelName: 'Seat',
+  });
+  return Seat;
+};
